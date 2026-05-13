002cba3e70f2acc89244911883e520ff9b10b0fc
Jixkls <cloudproducers@gmail.com>
2026-05-13 19:31:50 -0300

    Start

---
+++ src/config/env.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+
+const schema = z.object({
+  NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
+  PORT: z.string().default('3000'),
+  DATABASE_URL: z.string(),
+  JWT_SECRET: z.string(),
+});
+
+export const env = schema.parse(process.env);
